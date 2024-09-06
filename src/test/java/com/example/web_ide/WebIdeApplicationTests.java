6d0eaafcbb6f3b2be4dad298178a81a5489db3aa
nayou <nayoung4300@naver.com>
2024-09-07 02:49:08 +0900

    init

---
+++ src/test/java/com/example/web_ide/WebIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_ide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
